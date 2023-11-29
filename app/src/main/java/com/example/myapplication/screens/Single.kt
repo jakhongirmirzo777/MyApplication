b790d56aab8f0c812dd0882df3e0e4ae892e3b2d
jakhongirmirzo <jakhongirmirzo.tursunaliev@mail.ru>
2023-11-29 22:32:37 +0500

    Single page is in progress

--- app/src/main/java/com/example/myapplication/screens/Single.kt
+++ app/src/main/java/com/example/myapplication/screens/Single.kt
@@ -1,8 +1,27 @@
 package com.example.myapplication.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,14 +31,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import coil.compose.rememberImagePainter
 import com.example.myapplication.data.apiService
 import com.example.myapplication.data.entities.Product
+import com.example.myapplication.data.entities.ProductAdd
 import com.example.myapplication.data.entities.ProductData
+import com.google.accompanist.flowlayout.FlowRow
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SingleScreen(navController: NavHostController) {
@@ -67,8 +94,152 @@ fun SingleScreen(navController: NavHostController) {
             }
 
             else -> {
-                // Display the product title
-                Text(product?.title ?: "No title available")
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp)
+                ) {
+                    items(1) {
+                        IconButton(
+                            onClick = { navController.navigate("main") },
+                            modifier = Modifier
+                                .size(48.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = "sds",
+                                tint = Color.Gray
+                            )
+                        }
+
+                        Image(
+                            painter = rememberImagePainter(product!!.url),
+                            contentDescription = product!!.title,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .height(220.dp)
+                                .fillMaxWidth()
+                                .background(Color.White)
+                                .padding(bottom = 12.dp)
+                        )
+
+                        Text(
+                            product!!.title,
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color(android.graphics.Color.parseColor("#1F1F1F")),
+                            modifier = Modifier
+                                .padding(bottom = 16.dp)
+                        )
+                        Text(
+                            "Vegetable Family: ${product!!.type}",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(android.graphics.Color.parseColor("#616F7D")),
+                            modifier = Modifier
+                                .padding(bottom = 16.dp)
+                        )
+                        Text(
+                            product!!.description,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Color(android.graphics.Color.parseColor("#1F1F1F")),
+                            modifier = Modifier
+                                .padding(bottom = 16.dp)
+                        )
+                        Text(
+                            "Nutritions:",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Color(android.graphics.Color.parseColor("#FF005C")),
+                            modifier = Modifier
+                                .padding(bottom = 2.dp)
+                        )
+                        FlowRow(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 16.dp),
+                            mainAxisSpacing = 8.dp,
+                            crossAxisSpacing = 8.dp,
+                        ) {
+                            for (nutrition in product!!.text_list) {
+                                Text(
+                                    nutrition.value,
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color(android.graphics.Color.parseColor("#969696"))
+                                )
+                            }
+                        }
+
+                        Text(
+                            "${product!!.age} days passed since harvested",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Color(android.graphics.Color.parseColor("#FB8951")),
+                            modifier = Modifier
+                                .padding(bottom = 20.dp)
+                        )
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 30.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Price",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                color = Color(android.graphics.Color.parseColor("#1F1F1F")),
+                                modifier = Modifier.padding(end = 4.dp)
+                            )
+                            Text(
+                                text = "$${product!!.price}",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                color = Color(android.graphics.Color.parseColor("#1B327D")),
+                            )
+                        }
+
+                        Row(
+                            Modifier
+                                .fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            OutlinedButton(
+                                onClick = {
+
+                                },
+                                modifier = Modifier
+                                    .height(40.dp)
+                                    .width(150.dp)
+                                    .padding(end = 12.dp)
+                            ) {
+                                Text(
+                                    text = "Delete",
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color(android.graphics.Color.parseColor("#FF005C")),
+                                )
+                            }
+                            OutlinedButton(
+                                onClick = {
+
+                                },
+                                modifier = Modifier
+                                    .height(40.dp)
+                                    .width(150.dp)
+                            ) {
+                                Text(
+                                    text = "Edit",
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color(android.graphics.Color.parseColor("#FB8951"))
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
